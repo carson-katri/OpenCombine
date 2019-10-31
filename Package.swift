ad7dde2b75c40912731b0d02bc44cc29bb206135
Sergej Jaskiewicz <jaskiewiczs@icloud.com>
2019-10-31 14:42:13 +0300

    [COpenCombineHelpers] Support C++ 14
    The CI complains about missing headers, so let's reimplement std::string_view ourselves.

--- Package.swift
+++ Package.swift
@@ -3,33 +3,23 @@
 import PackageDescription
 
 let package = Package(
-  name: "OpenCombine",
-  products: [
-    .library(name: "OpenCombine", targets: ["OpenCombine"]),
-    .library(name: "OpenCombineDispatch", targets: ["OpenCombineDispatch"]),
-    .library(name: "OpenCombineFoundation", targets: ["OpenCombineFoundation"]),
-  ],
-  targets: [
-    .target(name: "COpenCombineHelpers"),
-    .target(
-      name: "OpenCombine",
-      dependencies: [
-        .target(name: "COpenCombineHelpers", condition: .when(platforms: [.macOS, .linux, .iOS])),
-      ],
-      exclude: [
-        "Publishers/Publishers.Encode.swift.gyb",
-        "Publishers/Publishers.Catch.swift.gyb",
-        "Publishers/Publishers.MapKeyPath.swift.gyb",
-      ]
-    ),
-    .target(name: "OpenCombineDispatch", dependencies: ["OpenCombine"]),
-    .target(name: "OpenCombineFoundation", dependencies: ["OpenCombine",
-                                                          "COpenCombineHelpers"]),
-    .testTarget(name: "OpenCombineTests",
-                dependencies: ["OpenCombine",
-                               "OpenCombineDispatch",
-                               "OpenCombineFoundation"],
-                swiftSettings: [.unsafeFlags(["-enable-testing"])]),
-  ],
-  cxxLanguageStandard: .cxx1z
+    name: "OpenCombine",
+    products: [
+        .library(name: "OpenCombine", targets: ["OpenCombine"]),
+        .library(name: "OpenCombineDispatch", targets: ["OpenCombineDispatch"]),
+        .library(name: "OpenCombineFoundation", targets: ["OpenCombineFoundation"]),
+    ],
+    targets: [
+        .target(name: "COpenCombineHelpers"),
+        .target(name: "OpenCombine", dependencies: ["COpenCombineHelpers"]),
+        .target(name: "OpenCombineDispatch", dependencies: ["OpenCombine"]),
+        .target(name: "OpenCombineFoundation", dependencies: ["OpenCombine",
+                                                              "COpenCombineHelpers"]),
+        .testTarget(name: "OpenCombineTests",
+                    dependencies: ["OpenCombine",
+                                   "OpenCombineDispatch",
+                                   "OpenCombineFoundation"],
+                    swiftSettings: [.unsafeFlags(["-enable-testing"])])
+    ],
+    cxxLanguageStandard: .cxx14
 )
