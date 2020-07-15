004653e6ee0cf2b5d02a6be88a533eef33457cae
Max Desiatov <max@desiatov.com>
2020-07-15 11:35:24 +0100

    Add the Runtime library dependency

--- Package.swift
+++ Package.swift
@@ -9,9 +9,14 @@ let package = Package(
         .library(name: "OpenCombineDispatch", targets: ["OpenCombineDispatch"]),
         .library(name: "OpenCombineFoundation", targets: ["OpenCombineFoundation"]),
     ],
+    dependencies: [
+      // Dependencies declare other packages that this package depends on.
+      // .package(url: /* package url */, from: "1.0.0"),
+      .package(url: "https://github.com/MaxDesiatov/Runtime.git", .branch("wasi-build")),
+    ],
     targets: [
         .target(name: "COpenCombineHelpers", cxxSettings: [.headerSearchPath(".")]),
-        .target(name: "OpenCombine", dependencies: ["COpenCombineHelpers"]),
+        .target(name: "OpenCombine", dependencies: ["COpenCombineHelpers", "Runtime"]),
         .target(name: "OpenCombineDispatch", dependencies: ["OpenCombine"]),
         .target(name: "OpenCombineFoundation", dependencies: ["OpenCombine",
                                                               "COpenCombineHelpers"]),
